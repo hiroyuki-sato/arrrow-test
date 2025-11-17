955c6e6e622423f9a9bd49ff8925327e427723ed
Hiroyuki Sato <hiroysato@gmail.com>
2025-11-17 13:30:58 +0900

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+exit 0
+##############################
+#
+##############################
+
 curl -L -O https://archive.apache.org/dist/arrow/arrow-22.0.0/apache-arrow-22.0.0.tar.gz
 
 tar xvfz apache-arrow-22.0.0.tar.gz
